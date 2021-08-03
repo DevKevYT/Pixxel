609fc60fb9e6889f56374d62b6cd3b9f57672920
Dev Kev <69854056+DevKevYT@users.noreply.github.com>
2021-08-03 13:17:10 +0200

    Delete mygdx directory

--- mygdx/entities/EntityEvents.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.entities;
-
-import com.badlogic.gdx.math.Vector2;
-import com.mygdx.objects.WorldObject;
-
-/**To generalize the entity behavior and to make sure*/
-public interface EntityEvents {
-    public void onHit(WorldObject source, Integer addHealth, Float knockbackX, Float knockbackY);
-}
